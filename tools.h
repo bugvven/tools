3bef5ea3ee9f53374f86b7c901e251c80e94bc11
bugvven <64295184+bugvven@users.noreply.github.com>
2021-07-07 11:10:15 +0800

    add tools.h

---
+++ tools.h
@@ -0,0 +1,14 @@
+﻿#ifndef TOOLS_H
+#define TOOLS_H
+
+#define count_of(a) \
+    ((int)(sizeof(a)/sizeof(a[0])))
+
+#define unused(a)\
+    ((void *)(&(a)));
+
+#define save_delete(__ptr)\
+    do{if(__ptr){delete __ptr; __ptr = NULL;}}while(0)
+
+
+#endif // TOOLS_H
